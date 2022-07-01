265313fbb0633724b75f43dee572d21b0575fc56
Marco André Mendes <marcoandre@gmail.com>
2022-07-01 08:55:39 -0300

    mudança na lingua e fuso horário

--- livraria/settings.py
+++ livraria/settings.py
@@ -93,11 +93,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = "pt-br"
-
 TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
-
 USE_TZ = True
 
 
